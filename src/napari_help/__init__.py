0948ce21656afdfb409128041d541c06a31841d5
Lorenzo Gaifas <brisvag@gmail.com>
2022-07-27 21:56:24 +0200

    fix versioning

--- src/napari_help/__init__.py
+++ src/napari_help/__init__.py
@@ -1,4 +1,8 @@
-__version__ = "0.0.1"
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
 from ._widget import ExampleQWidget, example_magic_widget
 
 __all__ = (
